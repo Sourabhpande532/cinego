7b4f4791adaf1ee7857829d580c8922109f0e957
Saurabh pande <93812514+Sourabhpande532@users.noreply.github.com>
2025-08-13 21:35:51 +0530

    Add: Home page

--- Js/pages/home.js
+++ Js/pages/home.js
@@ -1,7 +1,32 @@
 import { apiGetCinema } from "../api/apiGetCinema.js"
 
-const RenderMovieTable = (data)=>{
-    console.log(data)
+const displayData = ( { id, title, director, genre } ) => {
+    return `<tr> 
+       <td>${ id }</td>
+       <td>${ title }</td>
+       <td>${ director }</td>
+       <td>${ genre }</td>
+       <td><a href="details#${ id }">View</a></td>
+      </tr>`
+}
+
+const RenderMovieTable = ( data ) => {
+    const movieTable = data.map( displayData );
+    const movieRowHtml = movieTable.join( "" );
+    return `<table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Title</th>
+                <th>Director</th>
+                <th>Genre</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+        ${ movieRowHtml }
+        </tbody>
+    </table>`
 }
 
 export default async function home() {
