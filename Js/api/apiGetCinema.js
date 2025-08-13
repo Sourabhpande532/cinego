ec971ecbfd4300f5b19aa93119a8f46eb91d6483
Saurabh pande <93812514+Sourabhpande532@users.noreply.github.com>
2025-08-13 20:15:13 +0530

    Add: API to get schema data

---
+++ Js/api/apiGetCinema.js
@@ -0,0 +1,11 @@
+import axios from "https://cdn.jsdelivr.net/npm/axios@1.9.0/+esm";
+
+export const apiGetCinema = async () => {
+    try {
+        const { data } = await axios.get( "/api/movies" );
+        // Mirage returns { movies: [ … ] }
+        return { error: null, data: data.movies }
+    } catch ( err ) {
+        return { error: err, data: null }
+    }
+}
--- Js/app.js
+++ Js/app.js
@@ -1,3 +1,4 @@
 import router from "./route.js";
-
+import movieAPIs from "../Js/api/cinema.mock.server.js"
+movieAPIs();
 router.start()
