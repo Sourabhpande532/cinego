2204fa10ea4a5fc5da3c09257f007bb57f98121a
Saurabh pande <93812514+Sourabhpande532@users.noreply.github.com>
2025-08-14 21:09:25 +0530

    Add: add some api features delete

--- Js/pages/details.js
+++ Js/pages/details.js
@@ -1,9 +1,48 @@
+import { apiDeleteMovie } from "../api/apiDeleteCinema.js";
+import { apiGetMovieDetails } from "../api/apiGetMovieDetails.js";
+
 const getHash = () => {
     const getId = window.location.hash.replace( "#", "" );
     return Number( getId );
 }
-export default function render() {
+
+const deleteData = () => {
+    return `<h1>Data deleted </h1>`
+}
+
+const removeHandler = async () => {
     const id = getHash();
-    
-    document.getElementById( "app" ).innerHTML = cinegoDetails()
+    const { error } = await apiDeleteMovie( id );
+    // if ( error ) {
+    // }
+    document.getElementById( "app" ).innerHTML = deleteData();
+
+}
+
+const removeClickHandler = () => {
+    const removeDetails = document.getElementById( "remove-details" );
+    removeDetails.addEventListener( "click", removeHandler )
+}
+
+const cinegoDetails = ( data ) => {
+    return `<article>
+        <header>
+            <h2>${ data.title }</h2>
+        </header>
+        <body>
+            <p><strong>Director:</strong> ${ data.director }</p>
+            <p><strong>Genre:</strong> ${ data.genre }</p>
+            <p><strong>Date Watched:</strong> ${ data.dateWatched }</p>
+            <p><strong>Review:</strong> ${ data.review }</p>
+        </body>
+        <footer>
+            <button class='outline secondary' id="remove-details">Remove</button>
+        </footer>
+    </article>`
+}
+export default async function render() {
+    const id = getHash();
+    const { error, data } = await apiGetMovieDetails( id );
+    document.getElementById( "app" ).innerHTML = cinegoDetails( data );
+    removeClickHandler();
 }
